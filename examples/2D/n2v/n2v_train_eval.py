b7f9b7bca8ef87f7a1dccfabf403592105f75442
Ashesh <ashesh276@gmail.com>
2024-04-03 13:30:52 +0200

    n2v training

--- examples/2D/n2v/n2v_train_eval.py
+++ examples/2D/n2v/n2v_train_eval.py
@@ -28,11 +28,26 @@ from careamics.utils.experiment_saving import dump_config, add_git_info, get_wor
 from read_mrc import read_mrc  
 import numpy as np
 
+def noise_gen_decorator(data_func, poisson_noise_factor=-1, gaussian_noise_std=0.0):
+    def wrapper(*args, **kwargs):
+        noisy_data = data_func(*args, **kwargs)
+        if poisson_noise_factor > 0:
+            print('Enabling poisson noise with factor', poisson_noise_factor)
+            # The higher this factor, the more the poisson noise.
+            noisy_data = np.random.poisson(noisy_data / poisson_noise_factor)
+
+        if gaussian_noise_std > 0.0:
+            print('Adding gaussian noise', gaussian_noise_std)
+            noisy_data = noisy_data + np.random.normal(0, gaussian_noise_std, noisy_data.shape)
+        return noisy_data
+    return wrapper
+
 def custom_mrc_reader(fpath, axes):
     _, data = read_mrc(fpath)
     data = data[None]
     data = np.swapaxes(data, 0, 3)
     return data[...,0].copy()
+
 
 def get_model():
     model = CAREamicsModule(
@@ -45,7 +60,7 @@ def get_model():
     )
     return model
 
-def train(datapath, traindir, just_eval=False,modelpath=None):
+def train(datapath, traindir, just_eval=False,modelpath=None, poisson_noise_factor=-1, gaussian_noise_std=0.0):
     assert os.path.exists(datapath) #and os.path.isdir(datapath), f"Path {datapath} does not exist or is not a directory"
     # setting up the experiment.
     config = {'datapath':datapath, 'modelpath':modelpath, 'just_eval':just_eval}
@@ -58,7 +73,10 @@ def train(datapath, traindir, just_eval=False,modelpath=None):
     wandb.init(name=os.path.join(hostname, *exp_directory.split('/')[-2:]), dir=traindir, project="N2V", config=config)
 
     data_type = "custom" if datapath.endswith(".mrc") else "tiff"
-    read_source_func = custom_mrc_reader if data_type == "custom" else None
+    mrc_read_fn = noise_gen_decorator(custom_mrc_reader, poisson_noise_factor=poisson_noise_factor, 
+                                      gaussian_noise_std=gaussian_noise_std)
+    
+    read_source_func = mrc_read_fn if data_type == "custom" else None
     # loading/training the model and predicting
     model = get_model()
     if just_eval:
@@ -107,8 +125,9 @@ if __name__ == '__main__':
     parser.add_argument('--modelpath', type=str, default=None)
     parser.add_argument('--just_eval', action='store_true')
     parser.add_argument('--traindir', type=str, default=os.path.expanduser('~/training/N2V/'))
-    parser.add_argument('--add_gaussian_noise_std', type=float, default=0.0)
+    parser.add_argument('--gaussian_noise_std', type=float, default=0.0)
     parser.add_argument('--poisson_noise_factor', type=float, default=-1)
     args = parser.parse_args()
-    train(args.datapath, args.traindir, just_eval=args.just_eval, modelpath=args.modelpath)
+    train(args.datapath, args.traindir, just_eval=args.just_eval, modelpath=args.modelpath, 
+          poisson_noise_factor=args.poisson_noise_factor, gaussian_noise_std=args.gaussian_noise_std)
 
