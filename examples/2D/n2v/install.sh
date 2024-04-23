5f146fc59131a5c439f30af328a86f73f76a874c
Ashesh <ashesh276@gmail.com>
2024-04-23 20:05:08 +0200

    installation script

---
+++ examples/2D/n2v/install.sh
@@ -0,0 +1,8 @@
+mamba install pytorch torchvision pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install albumentations
+pip install matplotlib
+pip install careamics_portfolio
+pip install -e .
+mamba install lightning -c conda-forge
+pip install wandb
+pip install czifile
