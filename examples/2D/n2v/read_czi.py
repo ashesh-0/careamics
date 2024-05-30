2423a01d98666881966680c44c1562d87109ba9f
Ashesh <ashesh276@gmail.com>
2024-05-30 13:01:07 +0200

    czi reader adapted for stefania

--- examples/2D/n2v/read_czi.py
+++ examples/2D/n2v/read_czi.py
@@ -2,10 +2,15 @@ from czifile import imread as imread_czi
 import numpy as np
 
 def load_data(fpath, axes=None):
-    # (4, 1, 4, 22, 512, 512, 1)
     data = imread_czi(fpath)
-    clean_data = data[3, 0, [0, 2], ..., 0]
-    clean_data = np.swapaxes(clean_data[..., None], 0, 4)[0]
+    if len(data.shape) > 4:
+        # (4, 1, 4, 22, 512, 512, 1)
+        clean_data = data[3, 0, [0, 2], ..., 0]
+        clean_data = np.swapaxes(clean_data[..., None], 0, 4)[0]
+    else:
+        # just for iba dataset. (2,H,W,1) -> (2,H,W)
+        clean_data = data.squeeze()
+
     print('Loaded from', fpath, 'shape:', clean_data.shape)
     return clean_data
 
